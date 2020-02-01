5c5a7baac4d7b6e8c2f3a4ea8ace14c6fdef1490
ytebine07 <ytebine07@gmail.com>
2020-02-01 20:01:14 +0900

    毎フレームごとに算出してカットできる

--- set/003.py
+++ set/003.py
@@ -21,22 +21,33 @@ detector.setModelPath(os.path.join(
     execution_path, "../model/resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
 
-detections = []
 for file in tqdm(glob.glob("./input/*.png")):
-    # detections.append(detector.detectObjectsFromImage(input_image=os.path.join(
-    #    execution_path, "./input/" + os.path.basename(file)), output_image_path=os.path.join(execution_path, "./output/" + os.path.basename(file))))
-    im = Image.open(file)
-    im_crop = im.crop((200, 0, 812, 1088)).save(
-        "./output/" + os.path.basename(file), quality=100)
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(
+        execution_path, "./input/" + os.path.basename(file)), output_image_path=os.path.join(execution_path, "./output/" + os.path.basename(file)))
+
+    for d in detections:
+        if d["name"] == "person" and d["percentage_probability"] > 90:
+            x1 = d["box_points"][0]
+            y1 = d["box_points"][1]
+            x2 = d["box_points"][2]
+            y2 = d["box_points"][3]
+            center = (x1 + x2) // 2
+
+            nx1 = center - (612/2)
+            nx2 = center + (612/2)
+
+            im = Image.open(file)
+            im_crop = im.crop((nx1, 0, nx2, 1088)).save(
+                "./croped_output/" + os.path.basename(file), quality=100)
 
 
-for eachObject in detections:
-    for ea in eachObject:
-        if ea["name"] == 'person' and ea["percentage_probability"] > 90:
-            x1 = ea["box_points"][0]
-            y1 = ea["box_points"][1]
-            x2 = ea["box_points"][2]
-            y2 = ea["box_points"][3]
-            center = (x1 + x2) // 2  # 切り捨て
+# for eachObject in detections:
+#    for ea in eachObject:
+#        if ea["name"] == 'person' and ea["percentage_probability"] > 90:
+#            x1 = ea["box_points"][0]
+#            y1 = ea["box_points"][1]
+#            x2 = ea["box_points"][2]
+#            y2 = ea["box_points"][3]
+#            center = (x1 + x2) // 2  # 切り捨て
 
-    #print(eachObject["name"], " : ", eachObject["percentage_probability"])
+    # print(eachObject["name"], " : ", eachObject["percentage_probability"])
