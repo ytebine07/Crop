f659d3afd5e2691bff053d66dcc76ad4f3dad3e4
ytebine07 <ytebine07@gmail.com>
2020-02-01 09:21:51 +0900

    001.pyでオブジェクト検出ができる

---
+++ set/001.py
@@ -0,0 +1,22 @@
+from imageai.Detection import ObjectDetection
+import os
+import glob
+
+import tqdm
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(
+    execution_path, "../model/resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+
+detections = []
+for file in glob.glob("./input/*.png"):
+    print(os.path.basename(file))
+    detections.append(detector.detectObjectsFromImage(input_image=os.path.join(
+        execution_path, "./input/" + os.path.basename(file)), output_image_path=os.path.join(execution_path, "./output/" + os.path.basename(file))))
+
+for eachObject in detections:
+    print(eachObject["name"], " : ", eachObject["percentage_probability"])
