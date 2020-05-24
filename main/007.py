aae0f1d57737d767bca1a2ad473d451bbdd62ced
ytebine07 <ytebine07@gmail.com>
2020-05-24 20:50:21 +0900

    最初と最後の座標計算がおかしかった点を修正

--- main/007.py
+++ main/007.py
@@ -5,17 +5,17 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import tqdm
 import glob
 import os
-import requests
-from imageai.Detection import ObjectDetection
 import sys
+
 from tqdm import tqdm
-sys.path.append('/usr/local/lib/python3.6/lib-dynload')
-sys.path.append('/usr/local/lib/python3.6/site-packages')
-sys.path.append('/usr/local/lib/python36.zip')
-sys.path.append('/usr/local/lib/python3.6')
+from imageai.Detection import ObjectDetection
+
+sys.path.append("/usr/local/lib/python3.6/lib-dynload")
+sys.path.append("/usr/local/lib/python3.6/site-packages")
+sys.path.append("/usr/local/lib/python36.zip")
+sys.path.append("/usr/local/lib/python3.6")
 execution_path = os.getcwd()
 
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -23,15 +23,13 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 # 定義系
 
 AVERAGE_FRAME = int(os.getenv("AVERAGE_FRAME", 120))
-QUALITY = os.getenv("QUALITY", 'fastest')
+QUALITY = os.getenv("QUALITY", "fastest")
 
-model_path = os.getenv(
-    "MODEL_PATH", "./model/resnet50_coco_best_v2.0.1.h5")
-base_dir = os.getenv("BASE_DIR", "./data/set/")
+model_path = os.getenv("MODEL_PATH", "./model/resnet50_coco_best_v2.0.1.h5")
+base_dir = os.getenv("BASE_DIR", "./data")
 input_path = os.getenv("INPUT_PATH", base_dir + "/set/images/*png")
 output_path = os.getenv("OUTPUT_PATH", base_dir + "./image2/")
-c_output_path = os.getenv(
-    "C_OUTPUT_PATH", base_dir + "/set/croped/")  # cropされた画像の出力先
+c_output_path = os.getenv("C_OUTPUT_PATH", base_dir + "/set/croped/")  # cropされた画像の出力先
 positions_path = os.getenv("POSITIONS_PATH", base_dir + "/positions.txt")
 
 
@@ -52,14 +50,19 @@ def main():
     original_centers = []
     center = 960
     for file in tqdm(sorted(glob.glob(input_path)), desc="画像解析"):
-        input_image = os.path.join(execution_path, os.path.dirname(
-            input_path), os.path.basename(file))
+        input_image = os.path.join(
+            execution_path, os.path.dirname(input_path), os.path.basename(file)
+        )
         output_image_path = os.path.join(
-            execution_path, output_path, os.path.basename(file))
+            execution_path, output_path, os.path.basename(file)
+        )
 
         # ここでオブジェクト判別
         detections = detector.detectObjectsFromImage(
-            input_image=input_image, output_image_path=output_image_path, output_type='array')
+            input_image=input_image,
+            output_image_path=output_image_path,
+            output_type="array",
+        )
 
         # 複数のオブジェクトを検出する可能性があるのでforで回す
         for d in detections[1]:
@@ -78,13 +81,13 @@ def main():
                 # ritu = d["percentage_probability"] > 80
 
         # フレーム中に人間を検出出来なかったら、1コマ前に検出された座標を入れておく
-        if is_human == False:
+        if is_human is False:
             original_centers.append(center)
 
         frame_count += 1
         is_human = False
 
-    f = open(positions_path, 'w')
+    f = open(positions_path, "w")
     f_count = 1
     for line in original_centers:
         a = "{0},{1}\n".format(f_count, int(line))
@@ -93,28 +96,31 @@ def main():
     f.close
 
     # 移動平均の算出
-    conv = Convolve(AVERAGE_FRAME, original_centers)
+    a = np.array(original_centers)
+    conv = Convolve(AVERAGE_FRAME, a)
     y3 = conv.calculate()
 
     fc = 0
     for file in tqdm(sorted(glob.glob(input_path)), desc="ファイル書き出し"):
-        croped_output_image_path = os.path.join(execution_path,
-                                                c_output_path,
-                                                os.path.basename(file))
+        croped_output_image_path = os.path.join(
+            execution_path, c_output_path, os.path.basename(file)
+        )
 
-        n1 = y3[fc] - (612//2)
-        n2 = y3[fc] + (612//2)
+        n1 = y3[fc] - (612 // 2)
+        n2 = y3[fc] + (612 // 2)
 
         # ここで画像書き出し
         Image.open(file).crop((n1, 0, n2, 1080)).save(
-            croped_output_image_path, quality=100)
+            croped_output_image_path, quality=100
+        )
         fc += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     import sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
     from utils import Convolve, Common
 
     main()
--- utils/convolve.py
+++ utils/convolve.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Convolve:
-    def __init__(self, average, input):
+    def __init__(self, average: int, input: np.ndarray):
         """
         @args
             average : 移動平均を求めるフレーム数
@@ -15,27 +15,11 @@ class Convolve:
         self._average = average
         self._input = input
 
-    def calculate(self):
+    def calculate(self) -> np.ndarray:
+        mean = self._input.mean()
+        offseted_input = self._input - mean
 
-        v = np.ones(self._average)/self._average
-        y3 = np.convolve(self._input, v, mode='same')
+        v = np.ones(self._average) / float(self._average)
+        y3 = np.convolve(offseted_input, v, mode="same")
 
-        # 最初と最後の計算が上手くできないので、
-        # オリジナルの座標を入れておく
-        return_list = []
-        frame_count = 0
-        for i in self._input:
-            if frame_count <= self._average:
-                # 動画冒頭
-                return_list.append(self._input[frame_count])
-            elif len(self._input) - frame_count <= self._average:
-                # 動画最後
-                return_list.append(self._input[frame_count])
-            else:
-                return_list.append(int(y3[frame_count]))
-            frame_count += 1
-
-        return return_list
-
-    def hoge(self):
-        return "hogeeee"
+        return y3 + mean
