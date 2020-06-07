3fdbdcfa83b04722236a3ca262c7af2291242d98
ytebine07 <ytebine07@gmail.com>
2020-06-07 19:26:52 +0900

    不要ファイル削除

--- scripts/001_cut.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-ffmpeg -ss 105 -i "./data/i.mp4" -t 15 -c copy "./data/input.mp4"
--- scripts/002_createResource.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-source ./scripts/_settings.sh
-
-# create sound.mp4
-ffmpeg -i  $ORIGINAL_PATH -vn -acodec copy -map 0:1 "$ORIGINAL_DATA_DIR/sound.mp4"
-
-# create images
-ffmpeg -i $ORIGINAL_PATH -vcodec png "$IMAGE_DIR/image_%5d.png"
